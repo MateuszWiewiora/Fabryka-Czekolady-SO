88df7b62bd63e278c630f326cf928d1a3431f69b
Mateusz Wiewióra <matiwpmpl@wp.pl>
2025-12-14 23:43:08 +0100

    Implementacja podstawy pracownika - FIX_1

--- src/worker.cpp
+++ src/worker.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include "common.h"
 #include <string>
+#include "common.h"
 
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        std::cerr << "[PRACOWNIK] Błąd: Brak argumentu - numeru stanowiska"
+        std::cerr << "[PRACOWNIK] Błąd: Brak argumentu - numeru stanowiska" << std::endl;
+        return 1;
     }
 
     int id = std::stoi(argv[1]);
